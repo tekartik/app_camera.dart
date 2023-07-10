e30ab57d49970699d14c261dcd78198085273b68
Alexandre Roux <alex@tekartik.com>
2023-07-10 11:57:46 +0200

    dart3a branch

---
+++ app_camera_support/tool/basic_run_ci_dart.dart
@@ -0,0 +1,13 @@
+import 'package:dev_test/package.dart';
+import 'package:path/path.dart';
+
+Future main() async {
+  var topDir = join('..');
+  // Pure dart
+  for (var dir in <String>[
+    'camera_web',
+    'js_qr',
+  ]) {
+    await packageRunCi(join(topDir, dir));
+  }
+}
